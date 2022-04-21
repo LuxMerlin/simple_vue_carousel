9efbeaf3333e159fe35748ea5febcb206b6d513b
merlin <merlin@merlins-MacBook-Air.local>
2022-04-21 08:44:20 +0100

    carousel added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <carousel
+            @next="next"
+            @prev="prev">
+    <carousel-slide v-for="(slide, index) in slides" 
+                    :key="slide" 
+                    :index="index"
+                    :visiableSlide="visiableSlide"
+                    :direction="direction">
+      <img :src="slide"/>
+    </carousel-slide>
+  </carousel>
+  <div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Carousel from './components/Carousel.vue';
+import CarouselSlide from './components/CarouselSlide.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      slides: [
+        'https://picsum.photos/id/13/600/350',
+        'https://picsum.photos/id/423/600/350',
+        'https://picsum.photos/id/103/600/350',
+        'https://picsum.photos/id/23/600/350',
+        'https://picsum.photos/id/12/600/350',
+        'https://picsum.photos/id/523/600/350'
+      ],
+      visiableSlide: 0,
+      direction: 'left'
+    }
+  },
+  computed: {
+    slideLen(){
+      return this.slides.length - 1;
+    }
+  },
+      methods: {
+        next(){
+          if (this.visiableSlide >= this.slideLen) {
+            this.visiableSlide = 0;
+          }else{
+            this.visiableSlide++;
+          }
+          this.direction = 'left'
+        },
+        prev(){
+          if (this.visiableSlide <= 0) {
+            this.visiableSlide = this.slideLen;
+          }else{
+            this.visiableSlide--;
+          }
+          this.direction = 'right'
+        }
+    },
+  components : {
+    Carousel,
+    CarouselSlide,
   }
 }
 </script>
 
 <style>
-#app {
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  padding: 0;
+  margin: 0;
+}
+
+#app{
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="carousel">
+      <slot></slot>
+      <button @click="next" class="carousel_button next"><span class="iconify" data-icon="ant-design:arrow-right-outlined" style="color: #f3f6f4;" data-width="40" data-height="40"></span></button>
+      <button @click="prev" class="carousel_button prev"><span class="iconify" data-icon="ant-design:arrow-left-outlined" style="color: #f3f6f4;" data-width="40" data-height="40"></span></button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Carousel',
+    methods: {
+        next(){
+            this.$emit('next')
+        },
+        prev(){
+            this.$emit('prev')
+        }
+    }
+}
+</script>
+
+<style>
+    .carousel{
+        position: relative;
+        width: 600px;
+        height: 350px;
+        overflow: hidden;
+    }
+
+    .carousel_button {
+        position: absolute;
+        height: 40px;
+        width: 50px;
+        top: calc(50% - 20px);
+        background-color: rgba(0,0,0,0.3);
+        border: none;
+    }
+
+    .carousel_button:focus , .carousel_button:hover{
+        outline: none;
+        cursor: pointer;
+    }
+
+    .next{
+        right: 0;
+    }
+
+    .prev{
+        left: 0;
+    }
+
+
+</style>
